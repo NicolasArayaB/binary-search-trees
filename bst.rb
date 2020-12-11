ef65a267b3d79c0968fa3b055df12d948fbdbe5b
NicolasArayaB <nicolasaraya@gmail.com>
2020-12-11 16:26:46 -0300

    structure

---
+++ bst.rb
@@ -0,0 +1,100 @@
+# Build a Tree class which accepts an array when initialized. The Tree class should have a root
+#  attribute which uses the return value of #build_tree which you’ll write next.
+
+class Tree
+  def initialize
+    @root = nil
+  end
+
+# Method which takes an array of data and turns it into a balanced binary tree full of 
+# Node objects appropriately placed. The #build_tree method should return the level-1 root node.
+  
+  def build_tree(ary)
+    ary.sort.uniq!
+    ary.each do |element| 
+      new_node = new.node
+      new_node.value = element
+    end
+  end
+
+# Write an #insert and #delete method which accepts a value to insert/delete
+
+  def insert(value)
+
+  end
+
+  def delete(value)
+
+  end
+
+# Method which accepts a value and returns the node with the given value.
+
+  def find(value)
+
+  end
+
+# Method that returns an array of values. This method should traverse the tree in breadth-first 
+# level order. This method can be implemented using either iteration or recursion 
+# (try implementing both!). Tip: You will want to use an array acting as a queue to keep track of 
+# all the child nodes that you have yet to traverse and to add new ones to the list
+
+  def level_order
+
+  end
+
+# Write #inorder, #preorder, and #postorder methods that returns an array of values. 
+# Each method should traverse the tree in their respective depth-first order.
+
+  def inorder
+
+  end
+
+  def preorder
+
+  end
+
+  def postorder
+
+  end
+
+# Write a #height method which accepts a node and returns its height. 
+# Height is defined as the number of edges in longest path from a given node to a leaf node.
+
+  def height(node)
+
+  end
+
+# Write a #depth method which accepts a node and returns its depth. 
+# Depth is defined as the number of edges in path from a given node to the tree’s root node.
+
+  def depth(node)
+
+  end
+
+# Write a #balanced? method which checks if the tree is balanced. 
+# A balanced tree is one where the difference between heights of left subtree and right subtree 
+# of every node is not more than 1.
+
+  def balanced?
+
+  end
+
+# Write a #rebalance method which rebalances an unbalanced tree. 
+# Tip: You’ll want to create a level-order array of the tree before passing the array back into
+# the #build_tree method.
+
+  def rebalance
+
+  end
+end
+
+# Write a simple driver script that does the following:
+# 
+# 1. Create a binary search tree from an array of random numbers (`Array.new(15) { rand(1..100) }`)
+# 2. Confirm that the tree is balanced by calling `#balanced?`
+# 3. Print out all elements in level, pre, post, and in order
+# 4. try to unbalance the tree by adding several numbers > 100
+# 5. Confirm that the tree is unbalanced by calling `#balanced?`
+# 6. Balance the tree by calling `#rebalance`
+# 7. Confirm that the tree is balanced by calling `#balanced?`
+# 8. Print out all elements in level, pre, post, and in order
